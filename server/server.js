bc2f4824ccaec2213410ea208647cdc44082408d
martina <s273716@studenti.polito.it>
2020-10-17 10:31:55 +0200

    setup db and tables

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const dao = require('./dao.js');
 
 const PORT = 3001;
 app = new express();
@@ -10,8 +11,33 @@ app.use(express.static('client'));
 
 app.listen(PORT, ()=>console.log(`Server running on http://localhost:${PORT}/`));
 
-app.get('/api/test', (req, res) => {
+
+app.get('/', (req, res) => {
     res.json({
         "response": "ok"
     });
 });
+
+// REST API endpoints
+
+// Resources: Counters, Request_type
+
+// GET /counters
+// Request body: empty
+// Response body: Array of objects, each describing a Counter
+// Errors: none
+app.get('/counters', (req, res) => {
+    dao.listCounters()
+      .then((counters) => res.json(counters) )
+      .catch((err)=>res.status(503).json(dbErrorObj));
+  });
+
+  // GET /request type
+// Request body: empty
+// Response body: Array of objects, each describing a Request_type
+// Errors: none
+app.get('/request_type', (req, res) => {
+    dao.listRequests()
+      .then((requests) => res.json(requests) )
+      .catch((err)=>res.status(503).json(dbErrorObj));
+  });
