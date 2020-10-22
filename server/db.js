3e0846e895bd600d817efccafcb1d282aaec86ba
STEFANO CATALDI s277502 <s277502@studenti.polito.it>
2020-10-22 13:01:04 +0200

    fixed part

--- server/db.js
+++ server/db.js
@@ -1,17 +1,18 @@
 'use strict';
 
 const sqlite = require('sqlite3').verbose();
-
-
+const env = process.env.NODE_ENV;
+var DBSOURCE = env === 'test' ? './test/office-queue-test.db' :  './office-queue.db' ;
+/*
 if(process.env.NODE_ENV.localeCompare('test')){
   
     var DBSOURCE = "./test/office-queue-test.db";
     
-}
+} 
 else{
     var DBSOURCE = "./office-queue.db";     
 }
-   
+  */ 
 
 console.log('env=' + process.env.NODE_ENV)
 console.log('dbsource=' + DBSOURCE)
